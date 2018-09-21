e69fb8c9d6d4d0caab5b2fe215fa521b4b1fb054
yoshi1234 <hai_hai_hai_1229@yahoo.co.jp>
2018-09-21 21:44:05 +0900

    v-for

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var app = new Vue ({
     el: '#app',
     data: {
-        message: 'Hello Vue.js!'
+        message: 'Hello Vue.js!',
+        list: ['りんご', 'ばなな', 'いちご']        
+    },
+    methods: {
+        handleClick: function (event) {
+            alert(event.target)
+            alert('クリックしましたね！！')
+        }
     }
 })
